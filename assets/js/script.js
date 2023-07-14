093d1b3bf7034062bfecb1f6648243ab1926db36
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-07-14 12:55:16 -0500

    added fetch for popular games

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-fetch('https://api.rawg.io/api/platforms?key=87a21af0194d4db1a70bb42c6c104ebe', {
+const key = `87a21af0194d4db1a70bb42c6c104ebe`
+
+
+
+
+// RAWG API link/key
+fetch(`https://api.rawg.io/api/platforms?key=${key}`, {
   method: 'get'
 })
 .then(function(response){
@@ -6,13 +12,22 @@ fetch('https://api.rawg.io/api/platforms?key=87a21af0194d4db1a70bb42c6c104ebe', 
 })
 .then(function (data) {
   console.log(data);
-});
+})
+.catch(error => console.log(error));
 
 
 
-//Type in search, then run through API
+// Use RAWG API to attatch images to Popular games of month
 
-// search submit 
 
-let searchBar = document.querySelector('#SearchBar')
-let searchButton = document.querySelector('#searchButton')
+fetch(`https://api.rawg.io/api/games?key=${key}&dates=2023-06-01,2023-06-20&per_page=5&ordering=-added&`)
+.then(function(response){
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  data.array.forEach(popular => {
+    
+  });
+})
+.catch(error => console.log(error));
