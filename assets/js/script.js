f09c44b05075d1ed05b278806008f2ccac458f6e
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-14 12:13:34 -0500

    Cleaner Search Bar, and added event listener to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-fetch('https://api.rawg.io/api/platforms?key=87a21af0194d4db1a70bb42c6c104ebe', {
-  method: 'get'
+searchButton.addEventListener('click', function() {
+  fetch('https://api.rawg.io/api/platforms?key=87a21af0194d4db1a70bb42c6c104ebe', {
+    method: 'get'
+  })
+  .then(function(response){
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 })
-.then(function(response){
-  return response.json();
-})
-.then(function (data) {
-  console.log(data);
-});
+
 
 
 
